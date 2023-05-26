a0307b1a401535bddba2a63d6dd93b735203cb21
Jayson Tai <jayson.tai@hotmail.com>
2023-05-26 22:16:40 +1200

    add rotation implementation

--- src/robotNavigatorService.js
+++ src/robotNavigatorService.js
@@ -7,6 +7,25 @@ class RobotNavigatorService {
     robot.position.y = coordinate.y;
     table.layout[coordinate.x][coordinate.y] = robot;
   }
+
+  rotateRobot(robot, rotationDirection) {
+    const directions = [
+      DIRECTION.NORTH,
+      DIRECTION.EAST,
+      DIRECTION.SOUTH,
+      DIRECTION.WEST,
+    ];
+
+    const currentIndex = directions.indexOf(robot.direction);
+
+    if (rotationDirection === ROTATION.RIGHT) {
+      robot.direction = directions[(currentIndex + 1) % directions.length];
+    } else if (rotationDirection === ROTATION.LEFT) {
+      const newIndex =
+        (currentIndex - 1 + directions.length) % directions.length;
+      robot.direction = directions[newIndex];
+    }
+  }
 }
 
 export default RobotNavigatorService;
