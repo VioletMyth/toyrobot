27179fd4c5a23a54142c9d01b5fa958e51401100
Jayson Tai <jayson.tai@hotmail.com>
2023-05-28 19:59:43 +1200

    add command parser implementation

--- src/Parser.js
+++ src/Parser.js
@@ -1,4 +1,5 @@
 import DIRECTION from "./enums/Direction.js";
+import COMMAND from "./enums/command.js";
 
 class Parser {
   parseDirection(string) {
@@ -8,6 +9,41 @@ class Parser {
     } else {
       throw new Error(`Invalid direction: ${string}`);
     }
+  }
+
+  parseCommandName(string) {
+    const command = COMMAND[string];
+    if (command) {
+      return command;
+    } else {
+      throw new Error(`Invalid command: ${string}`);
+    }
+  }
+
+  parseCommand(string) {
+    const [commandName, commandArgs] = string.split(" ");
+
+    return commandName === "PLACE"
+      ? this.parsePlaceCommand(commandName, commandArgs)
+      : [this.parseCommandName(commandName), null];
+  }
+
+  parsePlaceCommand(commandName, placeArgs) {
+    if (!placeArgs) {
+      throw new Error(
+        'Invalid "place" command. Format should be "place x,y,f".'
+      );
+    }
+    const [x, y, f] = placeArgs.split(",");
+    if (!x || !y || !f) {
+      throw new Error(
+        'Invalid "place" command. Format should be "place x,y,f".'
+      );
+    }
+    return [
+      this.parseCommandName(commandName),
+      { x: parseInt(x), y: parseInt(y), direction: this.parseDirection(f) },
+    ];
   }
 }
 
--- tests/parser.test.js
+++ tests/parser.test.js
@@ -1,3 +1,4 @@
+import COMMAND from "../src/enums/command.js";
 import DIRECTION from "../src/enums/Direction.js";
 import Parser from "../src/Parser.js";
 
@@ -7,4 +8,45 @@ describe("Parser", () => {
     const direction = parser.parseDirection("NORTH");
     expect(direction).toBe(DIRECTION.NORTH);
   });
+
+  test("given string return command enum", () => {
+    const parser = new Parser();
+    const command = parser.parseCommandName("PLACE");
+    expect(command).toBe(COMMAND.PLACE);
+  });
+
+  test("given place command with raw arguments return place command enum with parsed arguments", () => {
+    const parser = new Parser();
+    const commandWithArgs = parser.parsePlaceCommand("PLACE", "1,1,NORTH");
+    expect(commandWithArgs).toStrictEqual([
+      COMMAND.PLACE,
+      { x: 1, y: 1, direction: "NORTH" },
+    ]);
+  });
+
+  test.each(["", "1", "1,1", "1,NORTH"])(
+    "given place command with no arguments should throw error",
+    (input) => {
+      const parser = new Parser();
+      expect(() => {
+        parser.parsePlaceCommand("PLACE", input);
+      }).toThrow();
+    }
+  );
+
+  test.each([
+    {
+      command: "PLACE 1,1,SOUTH",
+      expected: [COMMAND.PLACE, { x: 1, y: 1, direction: "SOUTH" }],
+    },
+    { command: "REPORT", expected: [COMMAND.REPORT, null] },
+    { command: "LEFT", expected: [COMMAND.LEFT, null] },
+  ])(
+    "given raw $command command string return command enum with parsed arguments",
+    ({ command, expected }) => {
+      const parser = new Parser();
+      const commandWithArgs = parser.parseCommand(command);
+      expect(commandWithArgs).toStrictEqual(expected);
+    }
+  );
 });
